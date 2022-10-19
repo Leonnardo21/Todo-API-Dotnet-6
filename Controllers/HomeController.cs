a98fcc2dea7e90a5033df99115901da3d77b5d50
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-10-19 10:16:50 -0300

    feat: Implemented update and delete post method.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,12 +13,42 @@ namespace Todo.Controllers
             return context.Todos.ToList();
         }
 
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id);
+        }
+
         [HttpPost("/")]
         public TodoModel Post([FromBody] TodoModel todo, [FromServices] AppDbContext context)
         {
             context.Add(todo);
             context.SaveChanges();
             return todo;
+        }
+
+        [HttpPut("/{id:int}")]
+        public TodoModel Put([FromRoute] int id, [FromBody] TodoModel todo, [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return todo;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+            context.Todos.Update(model);
+            context.SaveChanges();
+            return model;
+        }
+
+        [HttpDelete("/{id:int}")]
+        public TodoModel Delete([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+            return model;
         }
     }
 }
