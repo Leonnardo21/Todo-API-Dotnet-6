25294cf70248dedf2c72582dfe2fe3c53dcbb645
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-10-19 09:51:40 -0300

    feat: Implemented listing and post ]creation method

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public List<TodoModel> Get([FromServices] AppDbContext context)
+        {
+            return context.Todos.ToList();
+        }
+
+        [HttpPost("/")]
+        public TodoModel Post([FromBody] TodoModel todo, [FromServices] AppDbContext context)
+        {
+            context.Add(todo);
+            context.SaveChanges();
+            return todo;
+        }
+    }
+}
